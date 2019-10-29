52befd326adaebd1a61809341d3ee893bdc57da9
Christiaan Sommer <chris241@hotmail.nl>
2019-10-30 00:20:00 +0100

    Update project to have a basic editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+	<textarea ref="editor"></textarea>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import EasyMDE, { Options } from "easymde";
+
+@Component
+export default class Editor extends Vue
+{
+	markdownEditor: EasyMDE = {} as EasyMDE;
+
+	mounted()
+	{
+		this.markdownEditor = new EasyMDE({
+			element: this.$refs.editor,
+			initialValue: "# Hi \n Type something here"
+		} as Options);
+	}
+}
+</script>
+
+<style>
+  @import '~easymde/dist/easymde.min.css';
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Editor />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Editor from "@/components/Editor";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "home",
+	components: {
+		Editor
+	}
+};
 </script>
